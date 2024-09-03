163ced0ef4c81a6545bd597801fb7a8b6f42b311
botefori <botefori@gmail.com>
2024-09-03 02:14:49 +0200

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,31 +18,40 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.web_sg.security_group_id]
 
+  subnet_id = module.web_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow http and https in. Allow everything out"
-  vpc_id      = data.aws_vpc.default.id
-}
-
-
 
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   name = "web"
 
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.web_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
