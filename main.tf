222dec559afd2179e6c05f336f174169ac4e5d00
botefori <botefori@gmail.com>
2024-09-08 17:27:20 +0200

    fix: ingress remove optionnal cidr_block

--- main.tf
+++ main.tf
@@ -62,7 +62,6 @@ resource "aws_security_group" "web_vpc_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "web_vpc_ipv4" {
   security_group_id = aws_security_group.web_vpc_sg.id
-  cidr_ipv4         = module.web_vpc.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 443
