9cd9a7c611d9d888b0e540bba9bbf2c8c2292fd6
botefori <botefori@gmail.com>
2024-09-14 02:23:59 +0200

    fix: innapropriate value for cidr

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_security_group" "web_vpc_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "web_vpc_ipv4" {
   security_group_id = aws_security_group.web_vpc_sg.id
-  cidr_ipv4         = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
