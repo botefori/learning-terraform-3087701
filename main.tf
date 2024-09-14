b6d97d8092b4d62338f714860133723e8f1e5cb2
botefori <botefori@gmail.com>
2024-09-15 01:41:18 +0200

    fix: undeclared resource

--- main.tf
+++ main.tf
@@ -196,7 +196,7 @@ resource "aws_autoscaling_group" "web" {
   min_size             = 1
   max_size             = 2
   desired_capacity     = 1
-  launch_configuration = aws_launch_configuration.web_auto_scaling_attachment.name
+  launch_configuration = aws_launch_configuration.web_launch_config.name
   vpc_zone_identifier       = [aws_subnet.web_vpc_public_subnet_1_a.id, aws_subnet.web_vpc_public_subnet_1_b.id, aws_subnet.web_vpc_public_subnet_1_c.id]
 }
 
